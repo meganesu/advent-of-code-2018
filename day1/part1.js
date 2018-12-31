daa2281bab97f4113e4e81d3d548f74d380f3bbd
Megan Sullivan <meganesulli@gmail.com>
2018-12-30 21:43:25 -0800

    [Megan][Day 1] Completes part 1

--- day1/part1.js
+++ day1/part1.js
@@ -1,3 +1,13 @@
 const readFile = require('../helpers/readFile');
 const input = readFile();
 
+let frequency = 0;
+
+const lines = input.split('\n');
+for (const line of lines) {
+  const direction = (line[0] === '+') ? 1 : -1;
+  const value = parseInt(line.substring(1));
+  frequency += direction * value;
+}
+
+console.log(frequency);
